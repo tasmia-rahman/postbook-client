0d4378242ca67779d83d251a8ef6a029a7503775
tasmia-rahman <maliha1288@gmail.com>
2022-12-27 22:22:07 +0600

    header added

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="white" variant="white">
+            <Container>
+                <Navbar.Brand href="#home">Postbook</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className='ms-auto'>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/media">Media</Nav.Link>
+                        <Nav.Link href="/message">Message</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
