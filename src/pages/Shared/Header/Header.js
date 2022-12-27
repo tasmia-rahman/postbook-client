5b79f5bfe4f45476b17b776b1377808b67d0c6a0
tasmia-rahman <maliha1288@gmail.com>
2022-12-27 22:49:39 +0600

    header updated

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AuthContext } from './../../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+    const { user, logout } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logout()
+            .then(() => {
+                // Sign-out successful.
+            }).catch((error) => {
+                // An error happened.
+            })
+    }
+
     return (
-        <Navbar collapseOnSelect expand="lg" bg="white" variant="white">
+        <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
             <Container>
-                <Navbar.Brand href="#home">Postbook</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <h3>Postbook</h3>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className='ms-auto'>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/media">Media</Nav.Link>
-                        <Nav.Link href="/message">Message</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/signup">Sign Up</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav className="ms-auto">
+                        <Link className='nav-link' to='/'>Home</Link>
+                        <Link className='nav-link' to='/media'>Media</Link>
+                        <Link className='nav-link' to='/message'>Message</Link>
+                        <Link className='nav-link' to='/about'>About</Link>
+                    </Nav>
+                    <Nav>
+                        {
+                            user?.uid ?
+                                <>
+                                    <Link className='nav-link' onClick={handleLogOut}>Log out</Link>
+                                </>
+                                :
+                                <>
+                                    <Link className='nav-link' to='/signup'>Sign Up</Link>
+                                    <Link className='nav-link' to='/login'>Login</Link>
+                                </>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
